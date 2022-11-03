af09fa656c4d83c5b091edce20020d09edbc4bdc
TendTo <casablancaernesto@gmail.com>
2022-11-03 22:06:09 +0100

    refactor(start): simplify start command

--- module/start.py
+++ module/start.py
@@ -2,6 +2,6 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from module.shared import START_MESSAGE
 
-def start(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id, START_MESSAGE)
+
+def start(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(START_MESSAGE, quote=False)
