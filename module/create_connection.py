ce412a57ff5ac082676730326035e473e1814621
TendTo <casablancaernesto@gmail.com>
2022-11-04 02:57:54 +0100

    refactor(create_connection): use with statement

--- module/create_connection.py
+++ module/create_connection.py
@@ -1,40 +1,37 @@
 import sqlite3
-from sqlite3 import Error
 from module.shared import DB_PATH, DB_ERROR, INSERT, DELETE, SELECT
 from telegram.ext import CallbackContext
-from typing import Optional, Union
-
-def create_connection(db_file: str) -> sqlite3.Connection:
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(str(e))
-    return conn
+from typing import Literal, Optional, Union
 
 
-# pylint: disable=inconsistent-return-statements
-def connect_and_execute(context: CallbackContext, chat_id: int, query: str, params: tuple, operation: str) -> Optional[Union[int, list]]:
-    conn = create_connection(DB_PATH)
-    if not conn:
-        context.bot.send_message(chat_id, DB_ERROR)
-        return
+def connect_and_execute(
+    context: CallbackContext,
+    chat_id: int,
+    query: str,
+    params: tuple,
+    operation: Literal["insert", "delete", "select"],
+) -> Optional[Union[int, list]]:
+    with sqlite3.connect(DB_PATH) as conn:
 
-    cur = conn.cursor()
-    cur.execute(query, params)
-    if operation == INSERT:
-        conn.commit()
-        last_row_id = cur.lastrowid
-        conn.close()
-        return last_row_id
+        try:
+            cur = conn.cursor()
+            cur.execute(query, params)
+        except sqlite3.Error as e:
+            print(e)
+            context.bot.send_message(chat_id, DB_ERROR)
+            return None
 
-    if operation == DELETE:
-        conn.commit()
-        conn.close()
-        return
+        if operation == INSERT:
+            conn.commit()
+            last_row_id = cur.lastrowid
+            return last_row_id
 
-    if operation == SELECT:
-        rows = cur.fetchall()
-        conn.close()
-        return rows
-        
+        if operation == DELETE:
+            conn.commit()
+            return None
+
+        if operation == SELECT:
+            rows = cur.fetchall()
+            return rows
+
+    return None
